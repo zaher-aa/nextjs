4d0d37465f74ec6c179686e17c5de6c0a7e4e7c8
zaher-aa <zaherabuamro@gmail.com>
2022-06-08 16:04:54 +0300

    feat: added login and signup handlers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,9 @@
 const { sign } = require('jsonwebtoken');
 const { User } = require('../database/models');
 
-const loginHandler = async (req, res) => {
+const logInHandler = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  const { _doc: user } = await User.findOne({ email });
   if (!user) {
     return res.status(401).json({ message: 'User not found' });
   }
@@ -18,10 +18,35 @@ const loginHandler = async (req, res) => {
     if (err) {
       return res.status(500).json({ message: 'Error signing token' });
     }
-    return res.status(200).json({ token });
+    return res
+      .cookie('token', token)
+      .json({ message: 'Logged in successfully', data: userInfo });
+  });
+};
+
+const signUpHandler = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).json({ message: 'User already exists' });
+  }
+
+  const { _doc: newUser } = await User.create({ name, email, password });
+
+  const { password: pass, ...userInfo } = newUser;
+
+  sign(userInfo, process.env.JWT_SECRET, (err, token) => {
+    if (err) {
+      return res.status(500).json({ message: 'Error signing token' });
+    }
+    return res
+      .cookie('token', token)
+      .json({ message: 'User crated successfully', data: userInfo });
   });
 };
 
 module.exports = {
-  loginHandler,
+  logInHandler,
+  signUpHandler,
 };
